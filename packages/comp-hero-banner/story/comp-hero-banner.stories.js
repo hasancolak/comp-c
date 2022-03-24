4828cf47536ed28019c74c1fb1ea25461c6dfbce
Hasan Colak <hasan.colak@visual-meta.com>
2022-03-24 21:09:59 +0100

    init

--- packages/comp-hero-banner/story/comp-hero-banner.stories.js
+++ packages/comp-hero-banner/story/comp-hero-banner.stories.js
@@ -6,7 +6,7 @@ export default { title: 'Hero Banner' };
 export const primary = () => (
   <div>
     <HeroBanner 
-      img="./hero-banner.jpg" 
+      img="https://github.com/hasancolak/comp-c/blob/master/packages/comp-hero-banner/story/hero-banner.png" 
       headline="Say hello" 
       subHeadline="Sub header"/>
   </div>
